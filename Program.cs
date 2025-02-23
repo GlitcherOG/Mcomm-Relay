837cda04726931f217947b2a096aabfd6806d6d1
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-02-23 20:54:03 +1100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Mcomm_Relay
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Mcomm_Relay
 {
     internal class Program
     {
+        private TcpClient TCPClient = new TcpClient();
+        private UdpClient UDPClient = new UdpClient();
+        private UdpClient GameUDPClient = new UdpClient();
+
+        private Thread ThreadTCPListen;
+        private Thread ThreadUDPListen;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Connect to main server
+
+            //Main Server Listener
+            
+
+
+
+
+            UdpClient udpServer = new UdpClient(11000);
+            udpServer.ExclusiveAddressUse = false;
+            udpServer.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            //udpServer.Client.Bind();
         }
+
+
+        //host
+        //
     }
 }

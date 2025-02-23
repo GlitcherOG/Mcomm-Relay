8a1729dae7978f1e56edabd8132247f9b0b24685
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2025-02-23 20:23:41 +1100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Mcomm_Relay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
